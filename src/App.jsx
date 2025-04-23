a7cb451b1538241a5e211171c04185831d8ef922
lucie <lreyno202@caledonian.ac.uk>
2025-04-23 21:26:42 +0100

    added react

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react"; 
+
+function App() {
+    return (
+        <div className="App">
+        <h1>Hello, World!</h1>
+        <p>This is a simple React application.</p>
+        </div>
+    );
+    }
+
+export default App;
